3f2b2068a64acc0a2dc0a789164bc9e46b5e8c12
Corentin Foucrier <foucrier.corentin@gmail.com>
2019-09-23 13:58:13 +0200

    v0.1.1: Fix var in sh, add htaccess

--- start.sh
+++ start.sh
@@ -5,13 +5,13 @@ if [ -e .env ]; then
 fi
 
 echo "Please, choose a free port for access to the panel"
-echo "or leave blank for default port ${PORT}"
+echo "or leave blank for default port ${CONTAINER_PORT}"
 read userPort
 
 if [[ "$userPort" == "" ]]; then
-    echo "Your panel will be run on the default port ${PORT}"
+    echo "Your panel will be run on the default port ${CONTAINER_PORT}"
 else
-    sed -i -e "s/PORT=${PORT}/PORT=${userPort}/g" .env
+    sed -i -e "s/CONTAINER_PORT=${CONTAINER_PORT}/CONTAINER_PORT=${userPort}/g" .env
 fi
 
 docker-compose build
