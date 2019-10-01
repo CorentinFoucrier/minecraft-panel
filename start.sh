d20eb471c24993788a0f72dd546fac9a48b9c832
Corentin Foucrier <foucrier.corentin@gmail.com>
2019-10-01 22:26:26 +0200

    v0.1.3: "some changes env + start shell"

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ echo "or leave blank for default port ${CONTAINER_PORT}"
 read userPort
 
 if [[ "$userPort" == "" ]]; then
-    echo "Your panel will be run on the default port ${CONTAINER_PORT}"
+    echo "Your panel will be run on the default port [${CONTAINER_PORT}]"
 else
     sed -i -e "s/CONTAINER_PORT=${CONTAINER_PORT}/CONTAINER_PORT=${userPort}/g" .env
 fi
@@ -18,8 +18,8 @@ docker-compose build
 
 docker-compose -f docker-compose.yml up -d
 
-if [ "$DEV_ENV" == true ]; then
-    docker exec mc-panel coposer update
+if [ ${DEV_ENV} == true ]; then
+    docker exec ${CONTAINER_NAME} coposer update
 fi
 
 exit 0
