3de48a5e6b903dd1a2cc6cd97cfb6b97367855d5
CorentinFoucrier <foucrier.corentin@gmail.com>
2020-02-13 20:10:39 +0100

    v0.5.14: NodeJS socket.io refacto

--- www/nodejs/server.js
+++ www/nodejs/server.js
@@ -3,29 +3,16 @@ const io = require('socket.io')(http);
 const fs = require('fs');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-
-async function getTotalLines() {
-    const { stdout, stderr } = await exec('cat /var/minecraft_server/logs/latest.log | wc -l', { shell: true });
-    if (stderr) { console.error("error: " + stderr); }
-    return parseInt(stdout.replace("\\n", ""));
-}
-
-async function getLines(nb) {
-    const { stdout, stderr } = await exec('tail -n' + nb + ' /var/minecraft_server/logs/latest.log', { shell: true });
-    if (stderr) { console.error(stderr); }
-    return stdout.split(/[\r\n]+/);
-}
-
 let currentConnections = {};
+
 io.on('connection', (socket) => {
-    console.log('client connecté');
     currentConnections[socket.id] = { currentSocket: socket };
     currentConnections[socket.id].currentLine = 0;
 
     // At connection send 100 last lines to client...
     getLines(100)
         .then(lines => socket.emit('console', JSON.stringify(lines)));
-    // ...and store the totalLines number to currentLine link to current client
+    // ...and stores the number of "totalLines" to "currentLine" linked to the client (socket.id)
     getTotalLines().then(data => {
         currentConnections[socket.id].currentLine = data;
     });
@@ -48,15 +35,23 @@ io.on('connection', (socket) => {
         }
     });
 
-    socket.on('debug', (data) => {
-        console.log(currentConnections);
-    });
-
+    // Delete client socketId when a client disconnect
     socket.on('disconnect', () => {
-        console.log('client déconnecté');
         delete currentConnections[socket.id];
     });
 });
+
+const getTotalLines = async () => {
+    const { stdout, stderr } = await exec('cat /var/minecraft_server/logs/latest.log | wc -l', { shell: true });
+    if (stderr) { console.error("error: " + stderr); }
+    return parseInt(stdout.replace("\\n", ""));
+};
+
+const getLines = async (nb) => {
+    const { stdout, stderr } = await exec(`tail -n${nb} /var/minecraft_server/logs/latest.log`, { shell: true });
+    if (stderr) { console.error(stderr); }
+    return stdout.split(/[\r\n]+/);
+};
 
 const onFileChange = () => {
     getTotalLines().then(totalLines => {
@@ -70,14 +65,12 @@ const onFileChange = () => {
             }
         }
     }).catch(e => console.error(e));
-}
+};
 
 fs.watchFile(
     '/var/minecraft_server/logs/latest.log',
     { interval: 1500 },
-    function () {
-        onFileChange();
-    }
+    () => onFileChange()
 );
 
 http.listen(8000);
