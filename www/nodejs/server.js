cd583c3650b021b7e031e32402629500ffa491a9
CorentinFoucrier <foucrier.corentin@gmail.com>
2020-02-13 17:10:44 +0100

    v0.5.14: NodeJS socket.io refacto

--- www/nodejs/server.js
+++ www/nodejs/server.js
@@ -4,72 +4,80 @@ const fs = require('fs');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-io.on('connection', socket => {
-    let currentLine = 0;
+async function getTotalLines() {
+    const { stdout, stderr } = await exec('cat /var/minecraft_server/logs/latest.log | wc -l', { shell: true });
+    if (stderr) { console.error("error: " + stderr); }
+    return parseInt(stdout.replace("\\n", ""));
+}
 
-    socket.on('nodejs', data => {
+async function getLines(nb) {
+    const { stdout, stderr } = await exec('tail -n' + nb + ' /var/minecraft_server/logs/latest.log', { shell: true });
+    if (stderr) { console.error(stderr); }
+    return stdout.split(/[\r\n]+/);
+}
+
+let currentConnections = {};
+io.on('connection', (socket) => {
+    console.log('client connecté');
+    currentConnections[socket.id] = { currentSocket: socket };
+    currentConnections[socket.id].currentLine = 0;
+
+    // At connection send 100 last lines to client...
+    getLines(100)
+        .then(lines => socket.emit('console', JSON.stringify(lines)));
+    // ...and store the totalLines number to currentLine link to current client
+    getTotalLines().then(data => {
+        currentConnections[socket.id].currentLine = data;
+    });
+
+    socket.on('nodejs', (data) => {
         switch (data) {
             case "start":
                 currentLine = 0;
                 socket.broadcast.emit('client', "start");
                 break;
-            case "checkStatus":
-                socket.broadcast.emit('client', "checkStatus");
-                break;
             case "stop":
                 socket.broadcast.emit('client', "stop");
                 break;
+            case "checkStatus":
+                socket.broadcast.emit('client', "checkStatus");
+                break;
+            case "getVersion":
+                socket.broadcast.emit('getVersion', data);
+                break;
         }
     });
 
-    const main = () => {
-        /**
-         * @return {number} Number of lines
-         */
-        const getTotalLines = async () => {
-            const { stdout, stderr } = await exec('cat /var/minecraft_server/logs/latest.log | wc -l', { shell: true });
-            if (stderr) { console.error("error: " + stderr); }
-            return stdout;
-        }
-
-        const getLines = async nb => {
-            const { stdout, stderr } = await exec('tail -n' + nb + ' /var/minecraft_server/logs/latest.log', { shell: true });
-            if (stderr) { console.error(stderr); }
-            return stdout.split(/[\r\n]+/);
-        }
-
-        getTotalLines()
-            .then(totalLines => {
-                if (currentLine !== 0 && currentLine < totalLines) {
-                    getLines(totalLines - currentLine)
-                        .then(lines => {
-                            currentLine = totalLines;
-                            socket.emit('console', JSON.stringify(lines));
-                        }).catch(error => {
-                            console.log("error: " + error);
-                        });
-                } else if (currentLine === 0) {
-                    getLines(100)
-                        .then(lines => {
-                            currentLine = totalLines;
-                            socket.emit('console', JSON.stringify(lines));
-                        }).catch(error => {
-                            console.error("error: " + error);
-                        });
-                }
-            }).catch(error => {
-                console.error("error: " + error);
-            });
-    }
-
-    main();
-
-    fs.watchFile('/var/minecraft_server/logs/latest.log', { interval: 1500 }, () => {
-        setTimeout(() => {
-            main();
-        }, 500);
+    socket.on('debug', (data) => {
+        console.log(currentConnections);
     });
 
+    socket.on('disconnect', () => {
+        console.log('client déconnecté');
+        delete currentConnections[socket.id];
+    });
 });
+
+const onFileChange = () => {
+    getTotalLines().then(totalLines => {
+        for (const socketId in currentConnections) {
+            let currentLine = currentConnections[socketId].currentLine;
+            if (currentLine < totalLines) {
+                getLines(totalLines - currentLine).then(lines => {
+                    currentConnections[socketId].currentLine = totalLines;
+                    io.to(socketId).emit('console', JSON.stringify(lines));
+                }).catch(e => console.error(e));
+            }
+        }
+    }).catch(e => console.error(e));
+}
+
+fs.watchFile(
+    '/var/minecraft_server/logs/latest.log',
+    { interval: 1500 },
+    function () {
+        onFileChange();
+    }
+);
 
 http.listen(8000);
